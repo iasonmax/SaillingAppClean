784e7b44e283722c858a06a129ce4f425fde21fb
jason <jason>
2025-02-11 14:08:36 +0200

    Add project files.

---
+++ SaillingAppClean/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SaillingAppClean.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
